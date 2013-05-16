e40e6605347e755a3926e9677bdb44ac9d6012af
tavigo <tavigo@teleline.es>
2013-05-16 14:23:29 +0200

    Añadimos directorios de test

---
+++ xiu.sh
@@ -0,0 +1,5 @@
+touch ./src/test/java/.gitignore
+touch ./src/test/resources/.gitignore
+touch ./target/surefire/.gitignore
+touch ./target/archive-tmp/.gitignore
+touch ./target/test-classes/.gitignore
